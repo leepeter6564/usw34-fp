4baa94274aa689b86d318d8994bf1ed5d148bbf8
leepeter6564 <leepeter6564@hotmail.com>
2017-05-04 15:26:13 -0400

    revamped front page

--- index.php
+++ index.php
@@ -14,17 +14,22 @@
 
 <script>
 $(document).ready(function(){
-	$("#fade1").fadeIn( 2000 );
-	$("#fade2").delay( 2000 ).fadeIn(2000);
+	$("#fade1").delay( 2000 ).fadeIn( 2000 );
+	$("#fade2").delay( 4000 ).fadeIn( 2000 );
+	$("#fade3").delay( 6000 ).fadeIn( 2000 );
+
 });
 </script>
 
 <body>
 	<center>
-		<img src="header_pic.jpg" />
-		<div>
-		<h2> Welcome to the final project </h2>
-		</div>
+		<div style="height:400px">
+		<h2> USW34: Civil War, Final Project </h2>
+		<h4> By blah, blah, blah </h4>
+		<div class="parallax1"></div>
+
+
+		<div class="parallax1"></div>
 		<div id='fade1' style="display:none">
 			<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at varius velit, quis vulputate elit. Nullam viverra consectetur libero, aliquet imperdiet tellus pellentesque convallis. Ut et odio nisl. Nulla facilisi. Nulla facilisi. Phasellus rhoncus fermentum interdum. Nam mauris metus, porttitor at sem eget, sagittis imperdiet lectus. Duis luctus, diam eu egestas eleifend, turpis magna porttitor mauris, quis pharetra velit diam sit amet justo. Aenean at auctor lacus. Nullam non orci sit amet odio placerat cursus. Proin lobortis dictum elit in gravida. Nulla justo enim, pellentesque a facilisis in, dignissim vitae ante.
@@ -34,20 +39,27 @@ $(document).ready(function(){
 			Fusce laoreet maximus velit, at vulputate nibh. Nullam nec tempor ligula. Nullam ex lectus, semper sit amet odio vel, sagittis faucibus urna. Phasellus tortor tellus, porttitor mattis accumsan sed, elementum id diam. Nam vehicula ultricies risus ultricies condimentum. Ut gravida tempor massa sed accumsan. Morbi sed erat augue. Morbi id convallis eros. In nisi odio, aliquam et viverra quis, tincidunt ut nisl. Morbi molestie imperdiet odio in lacinia. Mauris viverra odio at nisi eleifend, sit amet vulputate nibh placerat. Curabitur malesuada mi a ex gravida, pharetra euismod tellus interdum. Aenean et bibendum mauris. Quisque maximus, erat eu faucibus blandit, leo quam aliquam odio, id lacinia velit diam et ligula. Donec vestibulum imperdiet elementum.
 			</p>
 		</div>
-		<div id='fade2' style='display:none'>
+		<div id='fade2' class="float_col" style="display:none;height:200px">
 			<p>
 			Fusce laoreet maximus velit, at vulputate nibh. Nullam nec tempor ligula. Nullam ex lectus, semper sit amet odio vel, sagittis faucibus urna. Phasellus tortor tellus, porttitor mattis accumsan sed, elementum id diam. Nam vehicula ultricies risus ultricies condimentum. Ut gravida tempor massa sed accumsan. Morbi sed erat augue. Morbi id convallis eros. In nisi odio, aliquam et viverra quis, tincidunt ut nisl. Morbi molestie imperdiet odio in lacinia. Mauris viverra odio at nisi eleifend, sit amet vulputate nibh placerat. Curabitur malesuada mi a ex gravida, pharetra euismod tellus interdum. Aenean et bibendum mauris. Quisque maximus, erat eu faucibus blandit, leo quam aliquam odio, id lacinia velit diam et ligula. Donec vestibulum imperdiet elementum.
 			</p>
-		
-			<p>
-			Fusce laoreet maximus velit, at vulputate nibh. Nullam nec tempor ligula. Nullam ex lectus, semper sit amet odio vel, sagittis faucibus urna. Phasellus tortor tellus, porttitor mattis accumsan sed, elementum id diam. Nam vehicula ultricies risus ultricies condimentum. Ut gravida tempor massa sed accumsan. Morbi sed erat augue. Morbi id convallis eros. In nisi odio, aliquam et viverra quis, tincidunt ut nisl. Morbi molestie imperdiet odio in lacinia. Mauris viverra odio at nisi eleifend, sit amet vulputate nibh placerat. Curabitur malesuada mi a ex gravida, pharetra euismod tellus interdum. Aenean et bibendum mauris. Quisque maximus, erat eu faucibus blandit, leo quam aliquam odio, id lacinia velit diam et ligula. Donec vestibulum imperdiet elementum.
-			</p>
-		</div>
-		<div>
 			<form action="/confed/1.php" class="inline">
-    			<button type="submit">Join the Confederacy!</button>
+    			<button type="submit" id="flag"><img src="union_flag.jpg"></button>
 			</form>
+			Join the Union!
+			<br>
+		</div>
 
+		
+		<div id='fade3' class="float_col" style="display:none;height:200px">
+			<p>
+			Fusce laoreet maximus velit, at vulputate nibh. Nullam nec tempor ligula. Nullam ex lectus, semper sit amet odio vel, sagittis faucibus urna. Phasellus tortor tellus, porttitor mattis accumsan sed, elementum id diam. Nam vehicula ultricies risus ultricies condimentum. Ut gravida tempor massa sed accumsan. Morbi sed erat augue. Morbi id convallis eros. In nisi odio, aliquam et viverra quis, tincidunt ut nisl. Morbi molestie imperdiet odio in lacinia. Mauris viverra odio at nisi eleifend, sit amet vulputate nibh placerat. Curabitur malesuada mi a ex gravida, pharetra euismod tellus interdum. Aenean et bibendum mauris. Quisque maximus, erat eu faucibus blandit, leo quam aliquam odio, id lacinia velit diam et ligula. Donec vestibulum imperdiet elementum.
+			</p>
+			<form action="/confed/1.php" class="inline">
+    			<button type="submit" id="flag"><img src="confed_flag.jpg"></button>
+			</form>
+			Join the Confederacy!
+			<br>
 		</div>
 		
 		<script src="application.js" type="text/javascript" charset="utf-8" async defer></script>
