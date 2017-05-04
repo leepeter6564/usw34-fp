54b9edfa2c7c6b7ebfe5e17529ba87e089df35f8
leepeter6564 <leepeter6564@hotmail.com>
2017-05-04 01:45:46 -0400

    experimenting; still have lots of stuff to do

--- index.php
+++ index.php
@@ -10,27 +10,46 @@
 
 </head>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<script>
+$(document).ready(function(){
+	$("#fade1").fadeIn( 2000 );
+	$("#fade2").delay( 2000 ).fadeIn(2000);
+});
+</script>
+
 <body>
 	<center>
+		<img src="header_pic.jpg" />
 		<div>
 		<h2> Welcome to the final project </h2>
 		</div>
-		<div>
+		<div id='fade1' style="display:none">
 			<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer at varius velit, quis vulputate elit. Nullam viverra consectetur libero, aliquet imperdiet tellus pellentesque convallis. Ut et odio nisl. Nulla facilisi. Nulla facilisi. Phasellus rhoncus fermentum interdum. Nam mauris metus, porttitor at sem eget, sagittis imperdiet lectus. Duis luctus, diam eu egestas eleifend, turpis magna porttitor mauris, quis pharetra velit diam sit amet justo. Aenean at auctor lacus. Nullam non orci sit amet odio placerat cursus. Proin lobortis dictum elit in gravida. Nulla justo enim, pellentesque a facilisis in, dignissim vitae ante.
-			</p>
-
+			</p1>
+		
 			<p>
 			Fusce laoreet maximus velit, at vulputate nibh. Nullam nec tempor ligula. Nullam ex lectus, semper sit amet odio vel, sagittis faucibus urna. Phasellus tortor tellus, porttitor mattis accumsan sed, elementum id diam. Nam vehicula ultricies risus ultricies condimentum. Ut gravida tempor massa sed accumsan. Morbi sed erat augue. Morbi id convallis eros. In nisi odio, aliquam et viverra quis, tincidunt ut nisl. Morbi molestie imperdiet odio in lacinia. Mauris viverra odio at nisi eleifend, sit amet vulputate nibh placerat. Curabitur malesuada mi a ex gravida, pharetra euismod tellus interdum. Aenean et bibendum mauris. Quisque maximus, erat eu faucibus blandit, leo quam aliquam odio, id lacinia velit diam et ligula. Donec vestibulum imperdiet elementum.
 			</p>
-
+		</div>
+		<div id='fade2' style='display:none'>
+			<p>
+			Fusce laoreet maximus velit, at vulputate nibh. Nullam nec tempor ligula. Nullam ex lectus, semper sit amet odio vel, sagittis faucibus urna. Phasellus tortor tellus, porttitor mattis accumsan sed, elementum id diam. Nam vehicula ultricies risus ultricies condimentum. Ut gravida tempor massa sed accumsan. Morbi sed erat augue. Morbi id convallis eros. In nisi odio, aliquam et viverra quis, tincidunt ut nisl. Morbi molestie imperdiet odio in lacinia. Mauris viverra odio at nisi eleifend, sit amet vulputate nibh placerat. Curabitur malesuada mi a ex gravida, pharetra euismod tellus interdum. Aenean et bibendum mauris. Quisque maximus, erat eu faucibus blandit, leo quam aliquam odio, id lacinia velit diam et ligula. Donec vestibulum imperdiet elementum.
+			</p>
+		
+			<p>
+			Fusce laoreet maximus velit, at vulputate nibh. Nullam nec tempor ligula. Nullam ex lectus, semper sit amet odio vel, sagittis faucibus urna. Phasellus tortor tellus, porttitor mattis accumsan sed, elementum id diam. Nam vehicula ultricies risus ultricies condimentum. Ut gravida tempor massa sed accumsan. Morbi sed erat augue. Morbi id convallis eros. In nisi odio, aliquam et viverra quis, tincidunt ut nisl. Morbi molestie imperdiet odio in lacinia. Mauris viverra odio at nisi eleifend, sit amet vulputate nibh placerat. Curabitur malesuada mi a ex gravida, pharetra euismod tellus interdum. Aenean et bibendum mauris. Quisque maximus, erat eu faucibus blandit, leo quam aliquam odio, id lacinia velit diam et ligula. Donec vestibulum imperdiet elementum.
+			</p>
+		</div>
+		<div>
 			<form action="/confed/1.php" class="inline">
     			<button type="submit">Join the Confederacy!</button>
 			</form>
 
 		</div>
 		
-		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 		<script src="application.js" type="text/javascript" charset="utf-8" async defer></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
